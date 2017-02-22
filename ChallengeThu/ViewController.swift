e064de72963679bd125083e75e961e8e5a6d5600
Riccardo Barba <rbarba2@sal-10.local>
2017-02-22 09:17:37 +0100

    Initial Commit

---
+++ ChallengeThu/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ChallengeThu
+//
+//  Created by Riccardo Barba on 22/02/17.
+//  Copyright © 2017 Riccardo Barba. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
